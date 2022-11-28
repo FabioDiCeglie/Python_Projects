ab0dde9e89c55f5483b5f57802f457f8d66f5155
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-28 21:44:55 +0100

    Django templates

--- WebProject/blog/urls.py
+++ WebProject/blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import Home
+from .views import Home, Article
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('articles', Article.as_view())
 ]
 
--- WebProject/blog/views.py
+++ WebProject/blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
 
-
+# def home(request):
+#     if request.method == "GET":
+#         return HttpResponse("Welcome to my blog")
+#     if request.method == "POST":
+#         return HttpResponse("POST BLOG")
 
 # Create your views here.
 class Home(View):
@@ -13,10 +19,28 @@ class Home(View):
         return HttpResponse("[POST]Welcome to my blog")
 
 
+class Article(View):
+    def get(self, request):
+        articles = [
+{
+"title": "Python 5 is officially announced",
+"category": "tech",
+"author": "Guido van Rossum",
+"content": "Just joking, we will have Python 3 for some years",
+"creation_date": datetime.now()
+},
+{
+"title": "Tesla goes bankrupt",
+"category": "auto",
+"author": "Elon Musk",
+"content": "Just trying to increase tesla share price here!",
+"created_at": datetime.now()
+}
+]
+
+        return render(request, "articles.html", {"articles": articles})
 
 
-# def home(request):
-#     if request.method == "GET":
-#         return HttpResponse("Welcome to my blog")
-#     if request.method == "POST":
-#         return HttpResponse("POST BLOG")
+
+
+
