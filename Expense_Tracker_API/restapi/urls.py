512f71d8adc11ccfc014c6b542a93b9464575c23
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-05 20:43:11 +0100

    added filtering support to listing view

--- Expense_Tracker_API/restapi/urls.py
+++ Expense_Tracker_API/restapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from restapi.views import ExpenseListCreate, ExpenseRetrieveDelete
 
 urlpatterns = [
-    path("expenses/", ExpenseListCreate.as_view(), name="expense-list-create"),
+    path("expenses", ExpenseListCreate.as_view(), name="expense-list-create"),
     path(
         "expenses/<pk>",
         ExpenseRetrieveDelete.as_view(),
