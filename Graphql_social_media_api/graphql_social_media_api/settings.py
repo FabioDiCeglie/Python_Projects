6beda9271512ac0f8e5894a4f644bd9269e2a371
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-06 20:12:07 +0100

    define first query with graphql

--- Graphql_social_media_api/graphql_social_media_api/settings.py
+++ Graphql_social_media_api/graphql_social_media_api/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "django_root.schema.schema"
+    "SCHEMA": "graphql_social_media_api.schema.schema"
 }
