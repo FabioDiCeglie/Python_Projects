4118407d24e3b526a6eadf6ad80664ac8fada714
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-07 22:07:12 +0100

    adding more data: users post

--- Graphql_social_media_api/graphql_social_media_api/schema.py
+++ Graphql_social_media_api/graphql_social_media_api/schema.py
@@ -6,8 +6,16 @@ class User(DjangoObjectType):
     class Meta:
         model = models.User
 
+class Post(DjangoObjectType):
+    class Meta:
+        model = models.Post
+
 class UserInput(graphene.InputObjectType):
     name = graphene.String()
+
+class PostInput(graphene.InputObjectType):
+    content = graphene.String()
+    user_id = graphene.Int()
 
 
 class CreateUser(graphene.Mutation):
@@ -27,8 +35,33 @@ class CreateUser(graphene.Mutation):
 
         return CreateUser(ok=True,user=instance)
 
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        input = PostInput(required=True)
+
+    ok = graphene.Boolean()
+    post = graphene.Field(Post)
+
+    @staticmethod
+    def mutate(root, info, input):
+        user = models.User.objects.get(pk=input.user_id)
+        if not models.User.objects.filter(pk=user.pk).exists():
+            return CreatePost(ok=False,post=None)
+
+        instance = models.Post(content=input.content,created_by=user)
+        try:
+            instance.save()
+        except Exception:
+            return CreatePost(ok=False, post= None)
+
+        return CreatePost(ok=True,post=instance)
 class Query(graphene.ObjectType):
+    users = graphene.List(User)
     user = graphene.Field(User, id=graphene.Int())
+
+    def resolve_users(self, info,  **kwargs):
+        return models.User.objects.all()
+
     def resolve_user(self, info,  **kwargs):
         id = kwargs.get("id")
 
@@ -40,5 +73,6 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
